be72e6cdf3e3e2ebad13f2bab9eaf0616758416b
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-06-26 19:09:14 +0100

    All logic is created and my calculator is ready to add styling

--- main.js
+++ main.js
@@ -1,22 +1,61 @@
-const keys = document.querySelector('.calculator-keys');
-const display = document.querySelector('.display')
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator-keys');
+const display = calculator.querySelector('.display')
+
 
 keys.addEventListener('click', event => {
   //this statements make sure if I click in between buttons it won't produce any results
   if (!event.target.closest('button')) return;
+
   const key = event.target;
   const keyValue = key.textContent;
   const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
 
-
-  if(key.classList.contains('number')) {
+// we're checking if this is a number or operator that is pressed
+  if(type === 'number') {
     if (displayValue === '0') {
+      display.textContent = keyValue;
+    } else if (previousKeyType === 'operator') {
       display.textContent = keyValue;
     } else {
       display.textContent = displayValue + keyValue;
     }
+    calculator.dataset.previousKeyType = 'number'
 }
+  
+  if (type === 'operator') {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach(el => {el.dataset.state = ''})
+    key.dataset.state = 'selected'
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === 'equal') {
+
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+    display.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+
+  calculator.dataset.previousKeyType = type;
 });
+
+function calculate (firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+  let result = '';
+  if (operator === 'plus') result =  firstNumber + secondNumber;
+  if (operator === 'minus') result = firstNumber - secondNumber
+  if (operator === 'times') result = firstNumber * secondNumber
+  if (operator === 'divide') result = firstNumber / secondNumber
+  return result
+}
+
 
 
 
